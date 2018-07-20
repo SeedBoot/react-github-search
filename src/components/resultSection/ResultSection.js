10a107c56ea4a2d825ffe019f1a6192e817e5ee6
SeedBoot <oliboots@gmail.com>
2018-07-20 15:08:09 +0100

    refining results

--- src/components/resultSection/ResultSection.js
+++ src/components/resultSection/ResultSection.js
@@ -4,8 +4,10 @@ import Tile from '../Tile/Tile';
 class ResultSection extends React.Component {
   render() {
     if (this.props.searchData) {
+      console.log('this.props.searchdata', this.props.searchData)
       let i;
       for (i in this.props.searchData) {
+        console.log('tile render:\n', this.props.searchData[i])
         const tiles = this.props.searchData[i].map((data) =>
           // const tiles = this.props.searchData.map((data) =>
           <Tile
